27f165d5128e8fa06857d95eedb0bfac06247594
erocrizs <aemielvin.loremia@gmail.com>
2016-01-24 20:08:22 +0800

    Add chevron symbols as dropdown indicators

--- res/js/content.js
+++ res/js/content.js
@@ -5,15 +5,19 @@ $(document).ready(function() {
 	var category_name = ["Personal Information", "Risk Factors", "Protective Factors"];
 	var category_children = ["#p-i-filter", "#r-f-filter", "#p-f-filter"];
 	$(".filter-bar h5").click(function() {
-		var text = $(this).text();
+		var text = $(this).text().trim();
 		for(var i=0; i<3; i++)
 		{
 			if(text===category_name[i])
 			{
-				if(category_clicked[i])
+				if(category_clicked[i]) {
 					$(category_children[i]).slideUp();
-				else
+					$(this).children("span").attr("class", "glyphicon glyphicon-chevron-down");
+				}
+				else{
 					$(category_children[i]).slideDown();
+					$(this).children("span").attr("class", "glyphicon glyphicon-chevron-up");
+				}
 
 				category_clicked[i] = !category_clicked[i];
 			}
