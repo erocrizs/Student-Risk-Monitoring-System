7e98e185dd5511b6b886c91d3cfd16181c0870cf
erocrizs <aemielvin.loremia@gmail.com>
2016-01-24 19:30:34 +0800

    Change identifiers used between categories from class to id

--- res/js/content.js
+++ res/js/content.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
 
 	var category_clicked = [false, false, false];
 	var category_name = ["Personal Information", "Risk Factors", "Protective Factors"];
-	var category_children = [".p-i-filter", ".r-f-filter", ".p-f-filter"];
+	var category_children = ["#p-i-filter", "#r-f-filter", "#p-f-filter"];
 	$(".filter-bar h5").click(function() {
 		var text = $(this).text();
 		for(var i=0; i<3; i++)
