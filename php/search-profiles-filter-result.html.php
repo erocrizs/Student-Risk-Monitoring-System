32e1ab4905f7075d1944de7e459169aa0615db1a
Vaan52 <123zachivanmartinez321@gmail.com>
2016-04-05 09:59:58 +0800

    Made it more readable

--- php/search-profiles-filter-result.html.php
+++ php/search-profiles-filter-result.html.php
@@ -1,26 +1,26 @@
 <!DOCTYPE html>
 
 <table class="table table-hover">
-<thead>
-	<th>ID</th>
-	<th>Name</th>
-	<th>Yr</th>
-	<th>Course</th>
-	<th>Sex</th>
-	<th>Age</th>
-	<th>Risks</th>
-</thead>
-<tbody>
-	<?php if(isset($filterResult)) : ?>
-		<?php while($row = $filterResult->fetch()) : ?>
-			<tr>
-				<td><input type="submit" class="btn" value=<?php echo $row['id']; ?> form="link-table"/></td>
-				<td><?php echo $row['surname'];
-								if($row['surname'] != '') : echo ', '; endif;
-								echo $row['firstname'];
-								if($row['firstname'] != '') : echo ' '; endif;
-								echo $row['mi'];
-								if($row['mi'] != '') : echo '.'; endif; ?></td>
+	<thead>
+		<th>ID</th>
+		<th>Name</th>
+		<th>Yr</th>
+		<th>Course</th>
+		<th>Sex</th>
+		<th>Age</th>
+		<th>Risks</th>
+	</thead>
+	<tbody>
+		<?php if(isset($filterResult)) : ?>
+			<?php while($row = $filterResult->fetch()) : ?>
+				<tr>
+					<td><input type="submit" class="btn" value=<?php echo $row['id']; ?> form="link-table"/></td>
+					<td><?php echo $row['surname'];
+							if($row['surname'] != '') : echo ', '; endif;
+							echo $row['firstname'];
+							if($row['firstname'] != '') : echo ' '; endif;
+							echo $row['mi'];
+							if($row['mi'] != '') : echo '.'; endif; ?></td>
 					<td><?php echo $row['yr']; ?></td>
 					<td><?php echo $row['course']; ?></td>
 					<td><?php if($row['gender'] == 1)  : echo 'Male';
