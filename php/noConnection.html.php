278f8e9203415f356bac92f0fc6706062c83c6f1
Vaan52 <123zachivanmartinez321@gmail.com>
2016-06-26 22:43:24 +0800

    Add noConnection
    for no connection from database if ever

---
+++ php/noConnection.html.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+		<title>Ateneo Strims</title>
+		<link href="/Student-Risk-Monitoring-System/res/css/bootstrap.min.css" rel="stylesheet" />
+		<link href="/Student-Risk-Monitoring-System/res/css/content.css" rel="stylesheet" />
+		
+		<script src="/Student-Risk-Monitoring-System/res/js/jquery-2.1.4.min.js"></script>
+		<script src="/Student-Risk-Monitoring-System/res/js/bootstrap.min.js"></script>
+	</head>
+	
+	<body>
+		<h1>NoConnection</h1>
+	</body>
+</html>
---
+++ php/noConnection.php
@@ -0,0 +1,3 @@
+<?php
+	include 'noConnection.html.php';
+?>
